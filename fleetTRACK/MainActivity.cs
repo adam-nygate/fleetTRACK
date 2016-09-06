e2e2b4537a996b5250e19a60dbb3857027ffa602
Adam Nygate <a.nygate@outlook.com>
2016-09-06 21:33:51 +0800

    Initial commit of solution structure

---
+++ fleetTRACK/MainActivity.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace fleetTRACK
+{
+    [Activity(Label = "fleetTRACK", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.btnLoggingToggle);
+
+            button.Click += delegate { button.Text = GetString(Resource.String.btnStopLogging); };
+        }
+    }
+}
+
